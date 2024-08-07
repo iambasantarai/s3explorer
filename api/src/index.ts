1da1509ad466be15ca10dedb3452ac82d204f4f2
iambasantarai <iambasantarai@gmail.com>
2024-08-07 22:37:57 +0545

    Migrate to typescript

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,13 @@
-const express = require('express');
+import express, {
+  type Application,
+  type Request,
+  type Response
+} from 'express';
 const PORT = 6942;
 
-const app = express();
+const app: Application = express();
 
-app.get('/heartbeat', (_req, res) => {
+app.get('/heartbeat', (_req: Request, res: Response) => {
   const hrtime = process.hrtime.bigint();
 
   res.status(200).json({ heartbeat: hrtime.toString() });
