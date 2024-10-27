b3549862d52666fdeef3eb26d7bbce909ba48b89
iambasantarai <iambasantarai@gmail.com>
2024-10-27 22:06:14 +0545

    Update zod import

--- api/src/schemas/directory.schema.ts
+++ api/src/schemas/directory.schema.ts
@@ -1,47 +1,51 @@
-import { object, string } from "zod";
+import { z } from "zod";
 
-const create = object({
-  body: object({
-    currentPath: string({
+const create = z.object({
+  body: z.object({
+    currentPath: z.string({
       required_error: "Current path is required.",
     }),
-    directoryName: string({
-      required_error: "Directory name is required.",
-    })
+    directoryName: z
+      .string({
+        required_error: "Directory name is required.",
+      })
       .min(1, "Directory name must contain at least 1 character.")
       .max(255, "Directory name cannot exceed 255 characters.")
       .regex(/^[^<>:"/\\|?*]+$/, "Directory name contains invalid characters."),
   }),
 });
 
-const update = object({
-  body: object({
-    currentPath: string({
+const update = z.object({
+  body: z.object({
+    currentPath: z.string({
       required_error: "Current path is required.",
     }),
-    oldDirectoryName: string({
-      required_error: "Old directory name is required.",
-    })
+    oldDirectoryName: z
+      .string({
+        required_error: "Old directory name is required.",
+      })
       .min(1, "Directory name must contain at least 1 character.")
       .max(255, "Directory name cannot exceed 255 characters.")
       .regex(/^[^<>:"/\\|?*]+$/, "Directory name contains invalid characters."),
-    newDirectoryName: string({
-      required_error: "New directory name is required.",
-    })
+    newDirectoryName: z
+      .string({
+        required_error: "New directory name is required.",
+      })
       .min(1, "Directory name must contain at least 1 character.")
       .max(255, "Directory name cannot exceed 255 characters.")
       .regex(/^[^<>:"/\\|?*]+$/, "Directory name contains invalid characters."),
   }),
 });
 
-const remove = object({
-  body: object({
-    currentPath: string({
+const remove = z.object({
+  body: z.object({
+    currentPath: z.string({
       required_error: "Current path is required.",
     }),
-    directoryName: string({
-      required_error: "Directory name is required.",
-    })
+    directoryName: z
+      .string({
+        required_error: "Directory name is required.",
+      })
       .min(1, "Directory name must contain at least 1 character.")
       .max(255, "Directory name cannot exceed 255 characters.")
       .regex(/^[^<>:"/\\|?*]+$/, "Directory name contains invalid characters."),
